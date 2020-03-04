7b0786338929f587cfb0e13d5e1bfe19705945b1
Rasha Moumneh <rasha@fastmail.com>
2020-03-04 18:45:24 -0500

    refactor Filters.js to be functional component

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,92 +1,86 @@
-import React, { Component } from 'react';
+import React, { useState, useContext } from 'react';
 import Checkbox from './Checkbox';
+import LocationContext from '../contexts/LocationContext';
 
-class Filters extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      neighborhoods: [
-        { value: 'West', isChecked: false },
-        { value: 'South', isChecked: false },
-        { value: 'Center City', isChecked: false },
-        { value: 'Southwest', isChecked: false }
-      ],
-      foodAssistance: [
-        { value: 'SNAP', isChecked: false },
-        { value: 'Philly Food Bucks', isChecked: false },
-        { value: 'Farmers Market Nutrition Program', isChecked: false }
-      ],
-      neighborhoodSelections: [],
-      foodAssistanceSelections: []
-    };
-  }
+const Filters = () => {
+  const { setFilters } = useContext(LocationContext);
 
-  handleCheckNeighborhoods = event => {
-    let neighborhoods = this.state.neighborhoods;
+  const [checkboxValues, setCheckboxValues] = useState({
+    neighborhoods: [
+      { value: 'West', isChecked: false },
+      { value: 'South', isChecked: false },
+      { value: 'Center City', isChecked: false },
+      { value: 'Southwest', isChecked: false }
+    ],
+    foodAssistance: [
+      { value: 'SNAP', isChecked: false },
+      { value: 'Philly Food Bucks', isChecked: false },
+      { value: 'Farmers Market Nutrition Program', isChecked: false }
+    ]
+  });
+
+  const handleCheckNeighborhoods = event => {
+    let neighborhoods = checkboxValues.neighborhoods;
     neighborhoods.forEach(neighborhood => {
       if (neighborhood.value === event.target.value)
         neighborhood.isChecked = event.target.checked;
     });
-    this.setState({ neighborhoods: neighborhoods });
-    console.log(this.state.neighborhoods);
-    const checkedValues = this.state.neighborhoods.filter(neighborhood => {
+    setCheckboxValues({ ...checkboxValues, neighborhoods: neighborhoods });
+    const checkedValues = neighborhoods.filter(neighborhood => {
       return neighborhood.isChecked === true;
     });
     const checkedNeighborhoods = checkedValues.map(value => {
       return value.value;
     });
 
-    this.setState({ neighborhoodSelections: checkedNeighborhoods });
+    console.log(checkedNeighborhoods);
   };
 
-  handleCheckFoodAssistance = event => {
-    let foodAssistance = this.state.foodAssistance;
+  const handleCheckFoodAssistance = event => {
+    let foodAssistance = checkboxValues.foodAssistance;
     foodAssistance.forEach(program => {
       if (program.value === event.target.value)
         program.isChecked = event.target.checked;
     });
-    this.setState({ foodAssistance: foodAssistance });
-    console.log(this.state.foodAssistance);
-    const checkedValues = this.state.foodAssistance.filter(program => {
+    setCheckboxValues({ ...checkboxValues, foodAssistance: foodAssistance });
+    const checkedValues = foodAssistance.filter(program => {
       return program.isChecked === true;
     });
     const checkedFoodAssistance = checkedValues.map(value => {
       return value.value;
     });
 
-    this.setState({ foodAssistanceSelections: checkedFoodAssistance });
+    console.log(checkedFoodAssistance);
   };
 
-  render() {
-    return (
-      <div>
-        <h2>Neighborhoods</h2>
-        <ul>
-          {this.state.neighborhoods.map((neighborhood, index) => {
-            return (
-              <Checkbox
-                key={index}
-                handleCheck={this.handleCheckNeighborhoods}
-                {...neighborhood}
-              />
-            );
-          })}
-        </ul>
-        <h2>Food Assistance Programs</h2>
-        <ul>
-          {this.state.foodAssistance.map((program, index) => {
-            return (
-              <Checkbox
-                key={index}
-                handleCheck={this.handleCheckFoodAssistance}
-                {...program}
-              />
-            );
-          })}
-        </ul>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <h2>Neighborhoods</h2>
+      <ul>
+        {checkboxValues.neighborhoods.map((neighborhood, index) => {
+          return (
+            <Checkbox
+              key={index}
+              handleCheck={handleCheckNeighborhoods}
+              {...neighborhood}
+            />
+          );
+        })}
+      </ul>
+      <h2>Food Assistance Programs</h2>
+      <ul>
+        {checkboxValues.foodAssistance.map((program, index) => {
+          return (
+            <Checkbox
+              key={index}
+              handleCheck={handleCheckFoodAssistance}
+              {...program}
+            />
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
 
 export default Filters;
