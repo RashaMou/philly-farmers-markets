b9094d84c5c169e1e83a6ab25c2f271736c92340
Rasha Moumneh <rasha@fastmail.com>
2020-03-05 14:05:45 -0500

    fix filters checkbox styling

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -2,15 +2,17 @@ import React from 'react';
 
 export const CheckBox = props => {
   return (
-    <li className='checkbox'>
-      <input
-        key={props.id}
-        onChange={props.handleCheck}
-        type='checkbox'
-        checked={props.isChecked}
-        value={props.value}
-      />{' '}
-      {props.value}
+    <li>
+      <label className='checkbox'>
+        <input
+          key={props.id}
+          onChange={props.handleCheck}
+          type='checkbox'
+          checked={props.isChecked}
+          value={props.value}
+        />{' '}
+        {props.value}
+      </label>
     </li>
   );
 };
