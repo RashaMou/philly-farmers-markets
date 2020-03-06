089f998207dfe3c4759e450c96901bcf4a80ae13
Rasha Moumneh <rasha@fastmail.com>
2020-03-06 14:09:43 -0500

    Finally got filters working

--- src/helpers/filterMarkets.js
+++ src/helpers/filterMarkets.js
@@ -1,51 +1,52 @@
 const filterMarkets = (markets, filters, cb) => {
-  // console.log('filters', filters);
+  let filteredByNeighborhood = [];
+  let neighborhoodsFilteredByFoodAssistance = [];
 
-  // let filteredMarketsArray = [];
-
-  // markets.forEach(market => {
-  //   const marketAttributes = Object.values(market.attributes); // array
-  //   filters.forEach(filter => {
-  //     if (marketAttributes.includes(filter)) {
-  //       filteredMarketsArray.push(market);
-  //     }
-  //   });
-  // });
-
-  let filteredMarketsArray = [];
-
+  // filter by neighborhoods
   markets.forEach(market => {
     const marketAttributes = Object.values(market.attributes);
     filters.forEach(filter => {
       if (marketAttributes.includes(filter)) {
-        filteredMarketsArray.push(market);
-      }
-      if (
-        (filter === 'ACCEPT_FMNP') &
-        (market.attributes.ACCEPT_FMNP === 'Y')
-      ) {
-        filteredMarketsArray.push(market);
-      }
-      if (
-        (filter === 'ACCEPT_SNAP_ACCESS') &
-        (market.attributes.ACCEPT_SNAP_ACCESS === 'Y')
-      ) {
-        filteredMarketsArray.push(market);
-      }
-      if (
-        (filter === 'ACCEPT_PHILLY_FOOD_BUCKS_') &
-        (market.attributes.ACCEPT_PHILLY_FOOD_BUCKS_ === 'Y')
-      ) {
-        filteredMarketsArray.push(market);
+        filteredByNeighborhood.push(market); // here we should have the markets filtered by neighborhood
       }
     });
-    return filteredMarketsArray;
   });
 
-  // removes duplicates
-  let unique = [...new Set(filteredMarketsArray)];
+  // if no filters by neighborhood present, add all the markets to the filteredByNeighborhood array
+  if (filteredByNeighborhood.length === 0) {
+    filteredByNeighborhood = [...markets];
+  }
 
-  cb(unique);
+  // filter filteredByNeighborhood array by food assistance
+  filteredByNeighborhood.forEach(market => {
+    console.log(
+      'market.attributes.ACCEPT_SNAP_ACCESS',
+      market.attributes.ACCEPT_SNAP_ACCESS
+    );
+    filters.forEach(filter => {
+      if (
+        filter === 'ACCEPT_SNAP_ACCESS' &&
+        market.attributes.ACCEPT_SNAP_ACCESS === 'Y'
+      ) {
+        neighborhoodsFilteredByFoodAssistance.push(market);
+      }
+      if (filter === 'ACCEPT_FMNP' && market.attributes.ACCEPT_FMNP === 'Y') {
+        console.log('market fired', market);
+        neighborhoodsFilteredByFoodAssistance.push(market);
+      }
+      if (
+        filter === 'ACCEPT_PHILLY_FOOD_BUCKS_' &&
+        market.attributes.ACCEPT_PHILLY_FOOD_BUCKS_ === 'Y'
+      ) {
+        neighborhoodsFilteredByFoodAssistance.push(market);
+      }
+    });
+  });
+
+  // if no food assistance filters exist, then send the filteredByNeighborhood array to the cb, otherwise send the neighborhoodsFilteredByFoodAssistance array to the callback
+  if (neighborhoodsFilteredByFoodAssistance !== 0) {
+    cb(neighborhoodsFilteredByFoodAssistance);
+  } else cb(filteredByNeighborhood);
 };
 
 export default filterMarkets;
