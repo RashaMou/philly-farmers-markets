3be54a5eeb2399c34b100ed7efbec15ae331cb79
Rasha Moumneh <rasha@fastmailteam.com>
2021-04-14 19:57:56 -0400

    change filters icon

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
 import React, { useState } from "react";
+import logo from "./assets/logo.png";
+import AboutModal from "./components/AboutModal";
 import FilterBar from "./components/FilterBar";
 import MarketsMap from "./components/MarketsMap";
-import AboutModal from "./components/AboutModal";
-import logo from "./assets/logo.png";
 
 function App() {
-  const [toggleModal, setToggleModal] = useState(false);
+    const [toggleModal, setToggleModal] = useState(false);
 
-  const modal = () => {
-    console.log("click");
-    setToggleModal(!toggleModal);
-  };
+    const modal = () => {
+        console.log("click");
+        setToggleModal(!toggleModal);
+    };
 
-  return (
-    <div className="App">
-      <header className="header">
-        <div className="logo-container">
-          <img className="logo" src={logo} alt="Logo" />
+    return (
+        <div className="App">
+            <header className="header">
+                <div className="logo-container">
+                    <img className="logo" src={logo} alt="Logo" />
+                </div>
+                <div className="question-container" onClick={modal}>
+                    <i onClick={modal} className="far fa-question-circle"></i>
+                </div>
+            </header>
+            <div>
+                <FilterBar />
+                {toggleModal && <AboutModal setToggleModal={setToggleModal} />}
+                <MarketsMap
+                    containerElement={<div style={{ height: `100vh` }} />}
+                    mapElement={<div style={{ height: `100%` }} />}
+                />
+            </div>
         </div>
-        <div className="question-container" onClick={modal}>
-          <i onClick={modal} className="far fa-question-circle"></i>
-        </div>
-      </header>
-      <div>
-        <FilterBar />
-        {toggleModal && <AboutModal setToggleModal={setToggleModal} />}
-        <MarketsMap
-          containerElement={<div style={{ height: `100vh` }} />}
-          mapElement={<div style={{ height: `100%` }} />}
-        />
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
--- src/components/FilterBar.js
+++ src/components/FilterBar.js
@@ -1,39 +1,39 @@
-import React, { useState, useContext } from "react";
+import React, { useContext, useState } from "react";
 import Sidebar from "react-sidebar";
-import Filters from "./Filters";
 import LocationContext from "../contexts/LocationContext";
+import Filters from "./Filters";
 
 const FilterBar = () => {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
-  const { setSelectedMarket } = useContext(LocationContext);
+    const [sidebarOpen, setSidebarOpen] = useState(false);
+    const { setSelectedMarket } = useContext(LocationContext);
 
-  const onSetSidebarOpen = (open) => {
-    setSidebarOpen(open);
-  };
+    const onSetSidebarOpen = (open) => {
+        setSidebarOpen(open);
+    };
 
-  return (
-    <div onClick={() => setSelectedMarket(null)}>
-      <Sidebar
-        sidebar={<Filters setSidebarOpen={setSidebarOpen} />}
-        open={sidebarOpen}
-        onSetOpen={onSetSidebarOpen}
-        styles={{ sidebar: { background: "white", width: 325 } }}
-      >
-        <h2
-          className="filters-menu-title"
-          onClick={() => setSidebarOpen(!sidebarOpen)}
-        >
-          Filters >>
-        </h2>
-        {/* <div
-          className="filter-icon"
-          onClick={() => setSidebarOpen(!sidebarOpen)}
-        >
-          <i class="fas fa-filter"></i>
-        </div> */}
-      </Sidebar>
-    </div>
-  );
+    return (
+        <div onClick={() => setSelectedMarket(null)}>
+            <Sidebar
+                sidebar={<Filters setSidebarOpen={setSidebarOpen} />}
+                open={sidebarOpen}
+                onSetOpen={onSetSidebarOpen}
+                styles={{ sidebar: { background: "white", width: 325 } }}
+            >
+                {/* <h2
+                    className="filters-menu-title"
+                    onClick={() => setSidebarOpen(!sidebarOpen)}
+                >
+                    Filters >>
+                </h2> */}
+                <div
+                    className="filter-icon"
+                    onClick={() => setSidebarOpen(!sidebarOpen)}
+                >
+                    <i class="fas fa-sliders-h"></i>
+                </div>
+            </Sidebar>
+        </div>
+    );
 };
 
 export default FilterBar;
