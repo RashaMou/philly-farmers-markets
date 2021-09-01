cba9fbbb454a1d096355c53541872cf9d2e9ba0e
Rasha Moumneh <rasha@fastmailteam.com>
2021-09-01 19:07:44 -0400

    Add markets and markers to map

--- src/App.js
+++ src/App.js
@@ -1,36 +1,12 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { arcGisPostRequest } from "./utils/getMarkets";
 import logo from "./assets/logo.png";
 import AboutModal from "./components/AboutModal";
-import { useMarketsState } from "./contexts/MarketsContext";
 // import FilterBar from "./components/FilterBar";
-// import PhillyMap from "./components/PhillyMap";
+import PhillyMap from "./components/PhillyMap";
 import "./styles/App.scss";
 
 function App() {
   const [toggleModal, setToggleModal] = useState(false);
-  let [state, dispatch] = useMarketsState();
-  let [error, setError] = useState(false);
-
-  useEffect(() => {
-    // TODO: handle errors
-    const getMarkets = async () => {
-      try {
-        let res = await axios(arcGisPostRequest);
-        if (res.status === 200) {
-          dispatch({ type: "ON_SUCCESS", data: res.data.features });
-          console.log(res.data.features);
-        }
-      } catch (error) {
-        // this doesn't work
-        dispatch({ type: "ON_FAILURE", data: error });
-        setError(true);
-        alert(error);
-      }
-    };
-    getMarkets();
-  }, []);
 
   const modal = () => {
     console.log("click");
@@ -48,19 +24,9 @@ function App() {
         </div>
       </header>
       <div>
-        {error && <div>ERROR</div>}
-        <ul>
-          {state.markets.map((market, index) => {
-            return <li key={index}>{market.attributes.NAME}</li>;
-          })}
-        </ul>
         {/* <FilterBar /> */}
         {toggleModal && <AboutModal setToggleModal={setToggleModal} />}
-        {/* <MarketsMap
-                    containerElement={<div style={{ height: `100vh` }} />}
-                    mapElement={<div style={{ height: `100%` }} />}
-                /> */}
-        {/* <PhillyMap /> */}
+        <PhillyMap />
       </div>
     </div>
   );
--- src/components/PhillyMap.js
+++ src/components/PhillyMap.js
@@ -1,54 +1,58 @@
-// import React, { useEffect, useContext } from "react";
-// import "leaflet/dist/leaflet.css";
-// import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-// import { MarketsContext } from "../contexts/MarketsContext";
-// import getMarkets from "../utils/getMarkets";
+import React, { useEffect } from "react";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { useMarketsState } from "../contexts/MarketsContext";
+import axios from "axios";
+import { arcGisPostRequest } from "../utils/getMarkets";
+import marketIcon from "../assets/marker.png";
 
-// const PhillyMap = () => {
-//   const {
-//     setMasterMarketsArray,
-//     setFilteredMarkets,
-//     filteredMarkets,
-//     selectedMarket,
-//     setSelectedMarket,
-//   } = useContext(MarketsContext);
+const PhillyMap = ({ markets }) => {
+  let [state, dispatch] = useMarketsState();
 
-//   //   useEffect(() => {
-//   //     getMarkets(setMasterMarketsArray, setFilteredMarkets);
-//   //     console.log("Philly Map");
-//   //   }, [filteredMarkets, setFilteredMarkets, setMasterMarketsArray]);
+  const marketMarker = L.icon({
+    iconUrl: marketIcon,
+    iconSize: [45, 55],
+    shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
+  });
 
-//   useEffect(() => {
-//     console.log("Philly Map");
-//   }, []);
+  useEffect(() => {
+    // TODO: handle errors
+    const getMarkets = async () => {
+      try {
+        let res = await axios(arcGisPostRequest);
+        if (res.status === 200) {
+          dispatch({ type: "ON_SUCCESS", data: res.data.features });
+        }
+      } catch (error) {
+        // this doesn't work
+        dispatch({ type: "ON_FAILURE", data: error });
+        alert(error);
+      }
+    };
+    getMarkets();
+  }, []);
 
-//   return (
-//     <MapContainer center={[39.952414, -75.146301]} zoom={12}>
-//       <TileLayer
-//         url="https://api.mapbox.com/styles/v1/rjmh/ckkbqic0c4fzt17ntqcy6bc43/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoicmptaCIsImEiOiJja2ticWgyYngwZWduMndwY2RjZmhtMjltIn0.QOeHWiCR4DqKvx-l5nzUvQ"
-//         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
-//       />
-//       {filteredMarkets.map((market, index) => {
-//         return (
-//           <Marker key={index}>
-//             position={[market.geometry.y, market.geometry.x]}
-//           </Marker>
-//           //   <Marker
-//           //     key={index}
-//           //     position={{ lat: market.geometry.y, lng: market.geometry.x }}
-//           //     onClick={() => {
-//           //       setSelectedMarket(market);
-//           //       setMapCenter({ lat: market.geometry.y, lng: market.geometry.x });
-//           //     }}
-//           //     icon={{
-//           //       url: require("../assets/marker3.png"),
-//           //       scaledSize: { width: 65, height: 65 },
-//           //     }}
-//           //   />
-//         );
-//       })}
-//     </MapContainer>
-//   );
-// };
+  return (
+    <MapContainer center={[39.952414, -75.146301]} zoom={12}>
+      <TileLayer
+        url="https://api.mapbox.com/styles/v1/rjmh/ckkbqic0c4fzt17ntqcy6bc43/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoicmptaCIsImEiOiJja2ticWgyYngwZWduMndwY2RjZmhtMjltIn0.QOeHWiCR4DqKvx-l5nzUvQ"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
+      />
+      {state.markets.map((market, index) => {
+        return (
+          <Marker
+            key={index}
+            position={[
+              parseFloat(market.geometry.y),
+              parseFloat(market.geometry.x),
+            ]}
+            icon={marketMarker}
+          ></Marker>
+        );
+      })}
+    </MapContainer>
+  );
+};
 
-// export default PhillyMap;
+export default PhillyMap;
