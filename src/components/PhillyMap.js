d6af7832c14d371c4e1a7f602ac8ff76cc91594e
Rasha Moumneh <rasha@fastmailteam.com>
2021-09-01 19:40:11 -0400

    Add popups to market markers

--- src/components/PhillyMap.js
+++ src/components/PhillyMap.js
@@ -12,7 +12,7 @@ const PhillyMap = ({ markets }) => {
 
   const marketMarker = L.icon({
     iconUrl: marketIcon,
-    iconSize: [45, 55],
+    iconSize: [55, 55],
     shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
   });
 
@@ -23,6 +23,7 @@ const PhillyMap = ({ markets }) => {
         let res = await axios(arcGisPostRequest);
         if (res.status === 200) {
           dispatch({ type: "ON_SUCCESS", data: res.data.features });
+          console.log(res.data.features);
         }
       } catch (error) {
         // this doesn't work
@@ -39,16 +40,36 @@ const PhillyMap = ({ markets }) => {
         url="https://api.mapbox.com/styles/v1/rjmh/ckkbqic0c4fzt17ntqcy6bc43/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoicmptaCIsImEiOiJja2ticWgyYngwZWduMndwY2RjZmhtMjltIn0.QOeHWiCR4DqKvx-l5nzUvQ"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
       />
-      {state.markets.map((market, index) => {
+      {state.filteredMarkets.map((market, index) => {
         return (
-          <Marker
-            key={index}
-            position={[
-              parseFloat(market.geometry.y),
-              parseFloat(market.geometry.x),
-            ]}
-            icon={marketMarker}
-          ></Marker>
+          market.attributes.ACTIVE === "Y" && (
+            <Marker
+              key={index}
+              position={[
+                parseFloat(market.geometry.y),
+                parseFloat(market.geometry.x),
+              ]}
+              icon={marketMarker}
+            >
+              <Popup>
+                <h4>
+                  <a href={market.attributes.MARKET_WEBSITE}>
+                    {market.attributes.NAME}
+                  </a>
+                </h4>
+                <div className="marketInfo">
+                  <ul>
+                    <li>
+                      {market.attributes.ADDRESS}, {market.attributes.ZIP}
+                    </li>
+                    <li>{market.attributes.TIME}</li>
+                    <li>{market.attributes.DAY}</li>
+                    <li>{market.attributes.MONTHS}</li>
+                  </ul>
+                </div>
+              </Popup>
+            </Marker>
+          )
         );
       })}
     </MapContainer>
